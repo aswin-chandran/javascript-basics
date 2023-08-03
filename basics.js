8b25af6457e90794a6ed0c91d4c662b5e6d41d1d
aswin chandran <aswin1998ply@gmail.com>
2023-08-03 20:22:01 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,50 @@
+// console.log('Welcome to Javascript')
+// var x=11;
+// var y=2;
+// var result=x/y;
+// console.log(result);
+// var result=x%y;
+// console.log(result);
+
+// //Objects in javascript
+//  let car={
+//     model:'alto',
+//     brant:'susuki',
+//     price:1000000
+//  }
+//  console.log(car);
+
+//  let person={
+//     eyecolor:'blue',
+//     height:176,
+//     weight:60
+//  }
+//  console.log(person.height);
+
+
+// //ARRAY in javascript
+
+// var arrayOne=['aswin','arjith',' amal']
+// console.log(arrayOne);
+
+// //PUSH
+// arrayOne.push('sam');
+// console.log(arrayOne);
+// //POP
+// arrayOne.pop('sam');
+// console.log(arrayOne);
+
+
+// function
+
+function add(a,b){
+    console.log(a+b);
+}
+
+add(2,4);
+
+
+
+
+
+
---
+++ loop.js
@@ -0,0 +1,18 @@
+//print first ten
+for(let i=0;i<10;i++)
+{
+    console.log(i+1)
+}
+
+var x=['mango','grape','orange'];
+for(let i=0;i<x.length;i++)
+{
+    console.log(x[i])
+}
+
+var x=['mango','grape','orange'];
+ let i=0;
+ while(i<x.length){
+    console.log(x[i])
+    i++;
+ }
